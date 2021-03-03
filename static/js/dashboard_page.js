4a0f6f5f403bd79daec3a891af9dbcf2e7e81c72
alphatw221 <alphatw22193@gmail.com>
2021-03-03 16:00:38 +0800

    123

--- static/js/dashboard_page.js
+++ static/js/dashboard_page.js
@@ -1,7 +1,7 @@
 var worksheet = new Vue({
     el: '#dashboard',
     data: {
-        isFetching:false,
+        isFetching:true,
         worksheets:[],
         dict:{},
         n:1,
@@ -14,6 +14,7 @@ var worksheet = new Vue({
         page:1,
         paginate_select:20,
         serial_number_input:null,
+        has_next_page:true,
     },
     created(){
         this.isFetching=true
@@ -24,21 +25,36 @@ var worksheet = new Vue({
             .then(res => {
                 this.dict=res.data
                 this.isFetching=false
+                document.onscroll=this.next_page
             }).catch(err => { console.error(err);   })
 
         }).catch(err => { console.error(err);   })
-
         
     },
     methods:{
         update_data(){
+            this.page=1
+            this.has_next_page=true
             axios.get('/search_worksheet/',{params:{serial_number:this.serial_number_input,squad:this.squad_select,status:this.status_select,
                 type1:this.type1_select,type2:this.type2_select,region:this.region_select,
                 project:this.project_select,page:this.page,size:this.paginate_select}})
             .then(res => {
                 this.worksheets=res.data
-
+                
             }).catch(err => { console.error(err);   })
+        },
+        next_page(){
+            if(window.scrollY>=document.body.scrollHeight-document.body.offsetHeight && this.has_next_page){
+                this.has_next_page=false
+                this.page+=1
+                axios.get('/search_worksheet/',{params:{serial_number:this.serial_number_input,squad:this.squad_select,status:this.status_select,
+                    type1:this.type1_select,type2:this.type2_select,region:this.region_select,
+                    project:this.project_select,page:this.page,size:this.paginate_select}})
+                .then(res => {
+                    this.worksheets=this.worksheets.concat(res.data)
+                    this.has_next_page=true
+                }).catch(err => { this.has_next_page=false})
+            }
         },
         modify(id){
             window.open('/update_worksheet_page/'+id)
@@ -52,6 +68,6 @@ var worksheet = new Vue({
                     location.reload()
                 }) .catch(err => { console.error(err);  })
             }
-        }
+        },
     }
 })
